36fb4c5caee46872e9a1ebf68db974a293e12e58
ElonMax <64552688+ElonMax404@users.noreply.github.com>
2020-08-18 23:02:57 +0300

    Added Stick Bugg links detection.

--- RickrollDetector/detector.py
+++ RickrollDetector/detector.py
@@ -3,7 +3,7 @@ import requests
 
 url = input('Input url to check: ')
 headers = {'User-Agent': '<yourUserAgent>'}
-keywords = ['Rick','Astley','Rick Astley - Never Gonna Give You Up (Video)','Official Rick Astley','Never gonna give you up','Never gonna let you down','Never gonna run around and desert you','Never gonna make you cry','Never gonna say goodbye','Never gonna tell a lie and hurt you']
+keywords = ['Rick','Astley','Rick Astley - Never Gonna Give You Up (Video)','Official Rick Astley','Never gonna give you up','Never gonna let you down','Never gonna run around and desert you','Never gonna make you cry','Never gonna say goodbye','Never gonna tell a lie and hurt you','Stick Bugged','Get Stick Bugged Lol']
 response = requests.get(url, headers=headers)
 
 def checker(resp):
@@ -11,7 +11,7 @@ def checker(resp):
     found_keywords = 0
     for el in keywords:
         if el.lower() in str(resp.content).lower():
-            print(f"Looks like a rickroll! I've found those keywords in this link: ")
+            print(f"Looks like a troll link! I've found those keywords in this link: ")
             break
     for el in keywords:
         if el.lower() in str(resp.content).lower():
@@ -19,7 +19,7 @@ def checker(resp):
             found_keywords = found_keywords+1
 
     if found_keywords == 0:
-        print(f"You can be sure that this isn't a rickroll!")
+        print(f"You can be sure that this isn't a troll link!")
 
 
 checker(response)
